48d3a577c2332140f994836db43b795cddd0c021
liang shan guang <1648266192@qq.com>
2018-03-25 21:54:55 +0800

    P69:出现异常的锁会被自动释放

---
+++ src/main/java/chapter2sync/P68ThreadA.java
@@ -0,0 +1,20 @@
+/***********************************************************
+ * @Description : 
+ * @author      : 梁山广(Laing Shan Guang)
+ * @date        : 2018/3/25 下午9:50
+ * @email       : liangshanguang2@gmail.com
+ ***********************************************************/
+package chapter2sync;
+
+public class P68ThreadA extends Thread {
+    private P68Service service;
+
+    public P68ThreadA(P68Service service) {
+        this.service = service;
+    }
+
+    @Override
+    public void run() {
+        service.testMethod();
+    }
+}
