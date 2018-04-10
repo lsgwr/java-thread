9f4ffc382163fbc4c71bbb03b32113f2fb35d128
liang shan guang <1648266192@qq.com>
2018-04-11 00:03:13 +0800

    P256了

---
+++ src/main/java/chapter5Timer/P254Run.java
@@ -0,0 +1,29 @@
+/***********************************************************
+ * @Description : 定时时间早于当前时间，直接运行
+ * @author      : 梁山广(Laing Shan Guang)
+ * @date        : 2018/4/10 下午10:59
+ * @email       : liangshanguang2@gmail.com
+ ***********************************************************/
+package chapter5Timer;
+
+import java.text.ParseException;
+import java.util.Date;
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class P254Run {
+
+    static public class MyTask extends TimerTask {
+        @Override
+        public void run() {
+            System.out.println("运行了！时间为：" + new Date().toLocaleString());
+        }
+    }
+
+    public static void main(String[] args) throws ParseException {
+        MyTask task = new MyTask();
+        Timer timer = new Timer();
+        System.out.println("当前时间：" + new Date().toLocaleString());
+        timer.schedule(task, 3000, 5000);
+    }
+}
---
+++ src/main/java/chapter5Timer/P255Run.java
@@ -0,0 +1,46 @@
+/***********************************************************
+ * @Description : 执行任务的时间延时，那么下一次任务的执行时间
+ *                以上一次任务的"开始时间"作为参考
+ * @author      : 梁山广(Laing Shan Guang)
+ * @date        : 2018/4/10 下午11:56
+ * @email       : liangshanguang2@gmail.com
+ ***********************************************************/
+package chapter5Timer;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class P255Run {
+    private static Timer timer = new Timer();
+    private static int runCount = 0;
+
+    static public class MyTask1 extends TimerTask {
+
+        @Override
+        public void run() {
+            try {
+                System.out.println("1 begin 执行啦！时间为：" + new Date().toLocaleString());
+                Thread.sleep(1000);
+                System.out.println("1   end 执行啦！时间为：" + new Date().toLocaleString());
+                runCount++;
+                if (runCount == 5) {
+                    timer.cancel();
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static void main(String[] args) throws ParseException {
+        MyTask1 task = new MyTask1();
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        String dateString = "2018-04-10 23:06:00";
+        Date dateRef = sdf.parse(dateString);
+        System.out.println("字符串时间为：" + dateRef.toLocaleString() + ", 当前时间为：" + new Date().toLocaleString());
+        timer.schedule(task, dateRef, 3000);
+    }
+}
