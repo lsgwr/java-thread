a736e45422cb64c11172681ebbf577013d43856e
liang shan guang <1648266192@qq.com>
2018-04-12 00:48:17 +0800

    看到第7章了

--- src/main/java/chapter6DesignPatterns/package-info.java
+++ src/main/java/chapter6DesignPatterns/package-info.java
@@ -1,5 +1,6 @@
 /*
  * Created By Liang Shan Guang at 2018/4/11 下午10:45
- * Description : 多线程的设计模式
+ * Description : 多线程的设计模式：同步方法、同步代码块、
+ *               静态内部类、静态代码块四种实现单例模式的方法
  */
 package chapter6DesignPatterns;
---
+++ src/main/java/chapter7More/package-info.java
@@ -0,0 +1,5 @@
+/*
+ * Created By Liang Shan Guang at 2018/4/12 上午12:46
+ * Description : 第7章：实力增补
+ */
+package chapter7More;
