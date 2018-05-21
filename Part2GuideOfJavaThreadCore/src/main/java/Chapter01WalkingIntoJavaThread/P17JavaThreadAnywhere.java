8012a565413c389782affbbf065caa80bb744316
liang shan guang <1648266192@qq.com>
2018-05-22 00:34:26 +0800

    看到P21了

---
+++ Part2GuideOfJavaThreadCore/src/main/java/Chapter01WalkingIntoJavaThread/P17JavaThreadAnywhere.java
@@ -0,0 +1,39 @@
+/***********************************************************
+ * @Description : 线程类的基本方法
+ * @author      : 梁山广(Laing Shan Guang)
+ * @date        : 2018/5/22 00:22
+ * @email       : liangshanguang2@gmail.com
+ ***********************************************************/
+package Chapter01WalkingIntoJavaThread;
+
+public class P17JavaThreadAnywhere {
+    public static void main(String[] args) {
+        System.out.println("The method is executed by thread : " + Thread.currentThread().getName());
+        Helper helper = new Helper("Java Thread AnyWhere");
+        // main中直接调用函数
+        helper.run();
+        // 创建线程取跑
+        Thread t = new Thread(helper);
+        t.start();
+
+    }
+
+    static class Helper implements Runnable {
+
+        private final String msg;
+
+        Helper(String msg) {
+            this.msg = msg;
+        }
+
+        private void doSomething(String msg) {
+            System.out.println("The 'doSomething' method was executed by thread : " + Thread.currentThread().getName());
+            System.out.println("Do something with " + msg);
+        }
+
+        @Override
+        public void run() {
+            doSomething(msg);
+        }
+    }
+}
