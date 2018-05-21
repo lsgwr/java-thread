74b69532c07f77f4610aa90c3fe182848cbfa621
liang shan guang <1648266192@qq.com>
2018-05-21 08:21:51 +0800

    Part2第一个程序

---
+++ Part2GuideOfJavaThreadCore/src/main/java/Chapter01WalkingIntoJavaThread/SimpleJavaApp.java
@@ -0,0 +1,18 @@
+/***********************************************************
+ * @Description : 
+ * @author      : 梁山广(Laing Shan Guang)
+ * @date        : 2018/5/21 08:21
+ * @email       : liangshanguang2@gmail.com
+ ***********************************************************/
+package Chapter01WalkingIntoJavaThread;
+
+import java.util.Date;
+
+public class SimpleJavaApp {
+    public static void main(String[] args) throws InterruptedException {
+        while (true) {
+            System.out.println(new Date());
+            Thread.sleep(1000);
+        }
+    }
+}
