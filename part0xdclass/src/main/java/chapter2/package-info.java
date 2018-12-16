83597871056b182e1a83f780f54f400972e36cce
liang shan guang <1648266192@qq.com>
2018-12-16 11:26:27 +0800

    add package-info

---
+++ part0xdclass/src/main/java/chapter2/package-info.java
@@ -0,0 +1,5 @@
+/*
+ * Created By Liang Shan Guang at 2018-12-15 20:59
+ * Description : 多线程基础
+ */
+package chapter2;
---
+++ part0xdclass/src/main/java/chapter3secutiry/package-info.java
@@ -0,0 +1,5 @@
+/*
+ * Created By Liang Shan Guang at 2018-12-15 20:59
+ * Description : 线程安全性
+ */
+package chapter3secutiry;
