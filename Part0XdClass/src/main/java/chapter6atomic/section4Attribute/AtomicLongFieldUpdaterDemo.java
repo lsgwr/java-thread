43aa111a0e4e8e7f8b3c10f2b68acbbb5b5b73bc
liang shan guang <1648266192@qq.com>
2019-08-05 00:35:03 +0800

    修改数组和引用

--- Part0XdClass/src/main/java/chapter6atomic/section4Attribute/AtomicLongFieldUpdaterDemo.java
+++ Part0XdClass/src/main/java/chapter6atomic/section4Attribute/AtomicLongFieldUpdaterDemo.java
@@ -1,5 +1,9 @@
 package chapter6atomic.section4Attribute;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.util.concurrent.atomic.AtomicLongFieldUpdater;
 import java.util.concurrent.atomic.AtomicReferenceFieldUpdater;
 
@@ -9,40 +13,29 @@ import java.util.concurrent.atomic.AtomicReferenceFieldUpdater;
 public class AtomicLongFieldUpdaterDemo {
 
     public static void main(String[] args) {
+        // 指定要更新的额字段: id
         AtomicLongFieldUpdater<Student> longFieldUpdater = AtomicLongFieldUpdater.newUpdater(Student.class, "id");
 
         Student xdclass = new Student(1L, "xdclass");
+        // 更新属性
         longFieldUpdater.compareAndSet(xdclass, 1L, 100L);
-        System.out.println("id="+xdclass.getId());
+        // 打印修改后的Id
+        System.out.println("id=" + xdclass.getId());
 
+        // 利用引用类型修改name属性
         AtomicReferenceFieldUpdater<Student, String> referenceFieldUpdater = AtomicReferenceFieldUpdater.newUpdater(Student.class, String.class, "name");
         referenceFieldUpdater.compareAndSet(xdclass, "xdclass", "wiggin");
-        System.out.println("name="+xdclass.getName());
+        System.out.println("name=" + xdclass.getName());
     }
 }
 
-class Student{
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+class Student {
+    /**
+     * 必须是volcatile的基础数据类型，不能是包装类哦！
+     */
     volatile long id;
     volatile String name;
-
-    public Student(Long id, String name) {
-        this.id = id;
-        this.name = name;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
 }
--- Part0XdClass/src/main/java/chapter6atomic/section5Reference/AtomicReferenceDemo.java
+++ Part0XdClass/src/main/java/chapter6atomic/section5Reference/AtomicReferenceDemo.java
@@ -1,5 +1,9 @@
 package chapter6atomic.section5Reference;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.util.concurrent.atomic.AtomicReference;
 
 /**
@@ -18,28 +22,10 @@ public class AtomicReferenceDemo {
     }
 }
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 class Student{
     private long id;
     private String name;
-
-    public Student(long id, String name) {
-        this.id = id;
-        this.name = name;
-    }
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
 }
