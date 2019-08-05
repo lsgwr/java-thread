6a1313a0b05307b08fe934ab527caf2fdf11e5d8
梁山广 <1648266192@qq.com>
2019-08-05 15:33:54 +0800

    Create DiscardOldestPolicyDemo.java

---
+++ Part0XdClass/src/main/java/chapter9pool/section5RejectPolicy/DiscardOldestPolicyDemo.java
@@ -0,0 +1,44 @@
+import java.lang.reflect.Field;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.ThreadPoolExecutor.DiscardOldestPolicy;
+
+public class DiscardOldestPolicyDemo {
+
+    private static final int THREADS_SIZE = 1;
+    private static final int CAPACITY = 1;
+
+    public static void main(String[] args) throws Exception {
+
+        // 创建线程池。线程池的"最大池大小"和"核心池大小"都为1(THREADS_SIZE)，"线程池"的阻塞队列容量为1(CAPACITY)。
+        ThreadPoolExecutor pool = new ThreadPoolExecutor(THREADS_SIZE, THREADS_SIZE, 0, TimeUnit.SECONDS,
+                new ArrayBlockingQueue<Runnable>(CAPACITY));
+        // 设置线程池的拒绝策略为"DiscardOldestPolicy"
+        pool.setRejectedExecutionHandler(new ThreadPoolExecutor.DiscardOldestPolicy());
+
+        // 新建10个任务，并将它们添加到线程池中。
+        for (int i = 0; i < 10; i++) {
+            Runnable myrun = new MyRunnable("task-"+i);
+            pool.execute(myrun);
+        }
+        // 关闭线程池
+        pool.shutdown();
+    }
+}
+
+class MyRunnable implements Runnable {
+    private String name;
+    public MyRunnable(String name) {
+        this.name = name;
+    }
+    @Override
+    public void run() {
+        try {
+            System.out.println(this.name + " is running.");
+            Thread.sleep(200);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
