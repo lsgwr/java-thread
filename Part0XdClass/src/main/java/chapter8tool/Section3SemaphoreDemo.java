9e6c3e25468d1c034365b1f5fa11800311e3a1b3
梁山广 <1648266192@qq.com>
2019-08-05 12:08:14 +0800

    Update Section3SemaphoreDemo.java

--- Part0XdClass/src/main/java/chapter8tool/Section3SemaphoreDemo.java
+++ Part0XdClass/src/main/java/chapter8tool/Section3SemaphoreDemo.java
@@ -7,7 +7,7 @@ public class Section3SemaphoreDemo {
     public static void main(String[] args) {
         Semaphore semaphore = new Semaphore(2);
 
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < 10; i++) { // 这里是10个线程，但是被Semaphore控制后，只有在前两个线程启动后才会启动后面8个
             new Thread(()->{
                 try {
                     semaphore.acquire();
