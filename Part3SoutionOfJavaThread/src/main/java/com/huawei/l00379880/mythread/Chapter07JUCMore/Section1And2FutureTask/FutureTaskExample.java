3e56129d90994a613ac140953a98b05e1e5ac4cb
梁山广 <1648266192@qq.com>
2019-09-04 11:01:07 +0800

    Create FutureTaskExample.java

---
+++ Part3SoutionOfJavaThread/src/main/java/com/huawei/l00379880/mythread/Chapter07JUCMore/Section1And2FutureTask/FutureTaskExample.java
@@ -0,0 +1,33 @@
+package com.huawei.l00379880.mythread.Chapter07JUCMore.Section1And2FutureTask;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.*;
+
+/***********************************************************
+ * @note      : FutureTask基本使用案例，Callable返回多线程执行结果
+ * @author    : l00379880 梁山广
+ * @version   : V1.0 at 2019/9/4 10:26
+ ***********************************************************/
+@Slf4j
+public class FutureTaskExample {
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        ExecutorService executorService = Executors.newCachedThreadPool();
+        // 创建callable任务，使用Future接收任务执行的结果、
+        FutureTask<String> future = new FutureTask<>(new Callable<String>() {
+            @Override
+            public String call() throws Exception {
+                System.out.println("do something in callable");
+                Thread.sleep(5000);
+                return "Done";
+            }
+        });
+        // 线程池可以直接传入FutureTask任务
+        executorService.execute(future);
+        System.out.println("Do something in main");
+        Thread.sleep(1000);
+        // 获取执行结果,get()方法是阻塞的，阻塞等待获取任务执行结果
+        System.out.println("Future执行结果：" + future.get());
+        executorService.shutdown();
+    }
+}
